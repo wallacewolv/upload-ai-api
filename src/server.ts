4bc63d85e01d3eb7f165b4fcda6c48d17592f817
wallace <wallace_wolv@hotmail.com>
2023-09-13 19:21:49 -0300

    feat: Configurando CORS para qualquer aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import { fastify } from "fastify";
-
+import { fastifyCors } from "@fastify/cors";
 import { createTranscriptionRoute } from "./routes/creat-transcription";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
