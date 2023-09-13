e48110e9bc6d994b82a06d172487089ab5dedc18
wallace <wallace_wolv@hotmail.com>
2023-09-13 18:44:41 -0300

    feat: Incluindo chamada para a openai para a transcrição

--- src/routes/creat-transcription.ts
+++ src/routes/creat-transcription.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
+import { createReadStream } from "node:fs";
 import { z } from "zod";
-import { prisma } from "prisma";
+
+import { openai } from "../lib/openai";
+import { prisma } from "../lib/prisma";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post("/videos/:videoId/transcription", async (req) => {
@@ -16,9 +19,24 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
     const { prompt } = bodySchema.parse(req.body);
 
-    return {
-      videoId,
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    const videoPath = video.path;
+    const audioReadStream = createReadStream(videoPath);
+
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: "whisper-1",
+      language: "pt",
+      response_format: "json",
+      temperature: 0,
       prompt,
-    };
+    });
+
+    return response.text;
   });
 }
